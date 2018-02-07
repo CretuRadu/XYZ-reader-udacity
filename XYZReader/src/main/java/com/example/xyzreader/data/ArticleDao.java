74458c61f5b9c12c47f163200cb1ee8940a9dcb9
Radu <j.creturadu@gmail.com>
2018-02-07 20:10:41 +0000

    Added room essentials

---
+++ XYZReader/src/main/java/com/example/xyzreader/data/ArticleDao.java
@@ -0,0 +1,18 @@
+package com.example.xyzreader.data;
+
+import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Delete;
+import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.Query;
+
+/**
+ * Created by xayru on 2/7/2018.
+ */
+@Dao
+public class ArticleDao {
+
+    public interface UserDao {
+
+    }
+
+}
