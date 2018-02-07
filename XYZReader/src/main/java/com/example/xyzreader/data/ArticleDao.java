6a7b2e0bd48b5bf9ca5a4c2036602b73ee3f573a
Radu <j.creturadu@gmail.com>
2018-02-07 20:24:04 +0000

    New functions in DAO

--- XYZReader/src/main/java/com/example/xyzreader/data/ArticleDao.java
+++ XYZReader/src/main/java/com/example/xyzreader/data/ArticleDao.java
@@ -3,7 +3,11 @@ package com.example.xyzreader.data;
 import android.arch.persistence.room.Dao;
 import android.arch.persistence.room.Delete;
 import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.OnConflictStrategy;
 import android.arch.persistence.room.Query;
+import android.arch.persistence.room.Update;
+
+import java.util.List;
 
 /**
  * Created by xayru on 2/7/2018.
@@ -13,6 +17,18 @@ public class ArticleDao {
 
     public interface UserDao {
 
+        @Query("SELECT * FROM articles")
+        List<Article> getAricles();
+
+        @Insert(onConflict = OnConflictStrategy.REPLACE)
+        public void insertUsers(Article... articles);
+
+        @Update
+        public void updateUsers(Article... articles);
+
+        @Delete
+        public void deleteUsers(Article... articles);
     }
+
 
 }
